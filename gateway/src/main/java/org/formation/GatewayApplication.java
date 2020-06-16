0066e56800b92fcbdd1b26e2f4d7a74848967b9d
David THIBAU <david.thibau@gmail.com>
2020-06-16 19:00:24 +0200

    Adding Turbine

--- gateway/src/main/java/org/formation/GatewayApplication.java
+++ gateway/src/main/java/org/formation/GatewayApplication.java
@@ -2,10 +2,12 @@ package org.formation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableCircuitBreaker
 public class GatewayApplication {
 
 	public static void main(String[] args) {
