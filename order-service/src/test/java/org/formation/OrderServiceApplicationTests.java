3bd6ec9842a32a03547cfda0ce12906d78b663a9
David THIBAU <david.thibau@gmail.com>
2020-12-13 11:47:47 +0100

    Ajout Order service

---
+++ order-service/src/test/java/org/formation/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
