debd0024c567bacf843c511be9a9d0c73b2f57d8
David THIBAU <david.thibau@gmail.com>
2021-02-21 12:20:54 +0100

    SCContract Producer

--- delivery-service/src/main/java/org/formation/resource/LivraisonResource.java
+++ delivery-service/src/main/java/org/formation/resource/LivraisonResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -61,7 +62,7 @@ public class LivraisonResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<Livraison> createLivraison(String noCommande) {
+	public ResponseEntity<Livraison> createLivraison(@RequestParam String noCommande) {
 
 		Livraison livraison = new Livraison();
 		livraison.setNoCommande(noCommande);
