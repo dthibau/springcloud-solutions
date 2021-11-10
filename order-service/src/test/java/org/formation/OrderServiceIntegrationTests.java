827376a136cea1f4722621a3c51afa906d49fbcf
David THIBAU <david.thibau@gmail.com>
2021-11-10 15:34:16 +0100

    Upgrade JUnit5

--- order-service/src/test/java/org/formation/OrderServiceIntegrationTests.java
+++ order-service/src/test/java/org/formation/OrderServiceIntegrationTests.java
@@ -5,8 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.formation.model.Client;
 import org.formation.model.Order;
 import org.formation.model.OrderItem;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,11 +14,9 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = OrderServiceApplication.class)
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
