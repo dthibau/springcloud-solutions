b5d1b16e4a5e6abf7a334cc93f949c6fab130f82
David THIBAU <david.thibau@gmail.com>
2023-11-21 17:07:12 +0100

    Tests from contract passed

--- delivery-service/src/main/java/org/formation/model/Livreur.java
+++ delivery-service/src/main/java/org/formation/model/Livreur.java
@@ -11,5 +11,31 @@ public class Livreur {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	private String nom,telephone;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	
 }
